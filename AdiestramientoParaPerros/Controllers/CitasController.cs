490b456cbbe880af8272e086c7d96d044d66f059
EstebanMQ <esteban.mq09@gmail.com>
2021-12-26 13:10:03 +0100

    commit

---
+++ AdiestramientoParaPerros/Controllers/CitasController.cs
@@ -0,0 +1,24 @@
+﻿using AdiestramientoParaPerros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdiestramientoParaPerros.Controllers
+{
+    public class CitasController : Controller
+    {
+        public IActionResult CitasIndex()
+        {
+            Calendario cal = new Calendario();
+            cal.DiasOcupados = new List<DateTime>();
+            cal.DiasOcupados.Add(new DateTime(2021, 12, 25));
+            cal.DiasOcupados.Add(new DateTime(2022, 01, 12));
+            cal.DiasOcupados.Add(new DateTime(2022, 01, 09));
+            cal.DiasOcupados.Add(new DateTime(2021, 12, 28));
+        
+            return View(cal);
+        }
+    }
+}

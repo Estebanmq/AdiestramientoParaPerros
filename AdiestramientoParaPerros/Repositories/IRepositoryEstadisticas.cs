94a0191c958271c32cca7dcb358da843e55cafda
EstebanMQ <esteban.mq09@gmail.com>
2022-02-18 14:24:15 +0100

    commit

---
+++ AdiestramientoParaPerros/Repositories/IRepositoryEstadisticas.cs
@@ -0,0 +1,17 @@
+﻿using AdiestramientoParaPerros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdiestramientoParaPerros.Repositories
+{
+    public interface IRepositoryEstadisticas
+    {
+        /// <summary>
+        /// Metodo que carga estadisticas
+        /// </summary>
+        /// <returns>El objeto con todas las estadisticas</returns>
+        Estadisticas GetEstadisticas();
+    }
+}
---
+++ AdiestramientoParaPerros/Repositories/RepositoryEstadisticas.cs
@@ -0,0 +1,37 @@
+﻿using AdiestramientoParaPerros.Data;
+using AdiestramientoParaPerros.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdiestramientoParaPerros.Repositories
+{
+    public class RepositoryEstadisticas : IRepositoryEstadisticas
+    {
+
+        private AdiestramientoContext context;
+
+        public RepositoryEstadisticas(AdiestramientoContext context)
+        {
+            this.context = context;
+        }
+
+        public Estadisticas GetEstadisticas()
+        {
+            Estadisticas estadisticas = new Estadisticas();
+            String procedimiento = "ESTADISTICAS @CONSULTASPENDIENTES OUT, @CONSULTASPROCESO OUT, @CONSULTASTERMINADAS OUT, @CANTIDADCLIENTES OUT, @CANTIDADEMPLEADOS OUT, @TOTALUSUARIOS OUT";
+            SqlParameter pamPendientes = new SqlParameter("@CONSULTASPENDIENTES", -1);
+            pamPendientes.Direction = System.Data.ParameterDirection.Output;
+            SqlParameter pamProceso = new SqlParameter("@CONSULTASPROCESO", -1);
+            pamProceso.Direction = System.Data.ParameterDirection.Output;
+            SqlParameter pamTerminadas = new SqlParameter("@CONSULTASTERMINADAS", -1);
+            pamTerminadas.Direction = System.Data.ParameterDirection.Output;
+            SqlParameter pamClientes = new SqlParameter("@CANTIDADCLIENTEs", -1);
+            pamClientes.Direction = System.Data.ParameterDirection.Output;
+            //Tienda, carrito, usuarios, dibujar component con menu, seguridad
+            return estadisticas;
+        }
+    }
+}
